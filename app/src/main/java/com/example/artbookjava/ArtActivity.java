f7426146d2bda092dcbc73af9b642e5cefbbf96d
Esma Hatun Akbulut <153107265+Esmahtn@users.noreply.github.com>
2025-08-20 10:27:14 +0300

    izin kontrolleri

--- app/src/main/java/com/example/artbookjava/ArtActivity.java
+++ app/src/main/java/com/example/artbookjava/ArtActivity.java
@@ -8,10 +8,13 @@ import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class ArtActivity extends AppCompatActivity {
 
@@ -26,14 +29,32 @@ public class ArtActivity extends AppCompatActivity {
             return insets;
         });
     }
-    public void save(View view){
+
+    public void save(View view) {
 
     }
-    public void selected(View view){
+
+    public void selected(View view) {
 
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
-            
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                Snackbar.make(view, "Permission needed for gallery", Snackbar.LENGTH_INDEFINITE).setAction("Give Permission", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //request permission
+                    }
+                }).show();
+            } else {
+                //request permission
+
+
+            }
+        } else {
+            /
+            //gallery
+
         }
         }
     }
